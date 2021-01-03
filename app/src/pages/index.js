c44005b00f5f15434d2dfca22ecc25d2bab5a12e
Verthon <christopher.sordyl@gmail.com>
2021-01-03 16:29:16 +0100

    feat: add some heroicons icons ;)

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import MenuIcon from "../components/MenuIcon"
 
 // markup
 const IndexPage = () => {
   return (
     <main className="container px-4 mx-auto">
       <title>Home Page</title>
+      <MenuIcon width="40"/>
     </main>
   )
 }
