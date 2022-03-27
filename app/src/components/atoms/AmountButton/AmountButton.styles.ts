6367c3b8f80498d35ce77993649fe3f560f9642c
Verthon <christopher.sordyl@gmail.com>
2022-03-27 09:22:34 +0200

    feat: add addtional min-max logic for amount button component

--- app/src/components/atoms/AmountButton/AmountButton.styles.ts
+++ app/src/components/atoms/AmountButton/AmountButton.styles.ts
@@ -1,5 +1,7 @@
 import styled, { css } from "styled-components"
 
+import { AmountButtonStylesProps } from "./AmountButton.types"
+
 const Wrapper = styled.div(
   ({ theme }) => css`
     display: inline-flex;
@@ -14,16 +16,32 @@ Wrapper.displayName = "AmountButtonWrapper"
 const Amount = styled.span``
 Amount.displayName = "AmountButtonAmount"
 
-const SignButton = styled.button`
-  border: 0;
-  background-color: inherit;
-  cursor: pointer;
-  padding: 0 1rem;
-`
-SignButton.displayName = "AmountSignButton"
+const MinusSignButton = styled.button<AmountButtonStylesProps>(
+  ({ theme, hasMinValue }) => css`
+    border: 0;
+    background-color: inherit;
+    cursor: ${hasMinValue ? "not-allowed" : "pointer"};
+    padding: 0 1rem;
+    color: ${hasMinValue ? theme.colors.blueGrey400 : "inherit"};
+  `
+)
+MinusSignButton.displayName = "AmountMinusSignButton"
+
+const PlusSignButton = styled.button<AmountButtonStylesProps>(
+  ({ theme, hasMaxValue }) => css`
+    border: 0;
+    background-color: inherit;
+    cursor: pointer;
+    cursor: ${hasMaxValue ? "not-allowed" : "pointer"};
+    padding: 0 1rem;
+    color: ${hasMaxValue ? theme.colors.blueGrey400 : "inherit"};
+  `
+)
+PlusSignButton.displayName = "AmountPlusSignButton"
 
 export const Styled = {
   Wrapper,
   Amount,
-  SignButton,
+  MinusSignButton,
+  PlusSignButton,
 }
--- app/src/components/atoms/AmountButton/AmountButton.tsx
+++ app/src/components/atoms/AmountButton/AmountButton.tsx
@@ -1,12 +1,14 @@
 import { Styled } from "./AmountButton.styles"
 import { AmountButtonProps } from "./AmountButton.types"
 
-export const AmountButton = ({ pizzaAmount, setPizzaAmount }: AmountButtonProps) => {
+export const AmountButton = ({ pizzaAmount, maxAmount = 5 }: AmountButtonProps) => {
+  const hasMinValue = pizzaAmount === 0
+  const hasMaxValue = pizzaAmount === maxAmount
   return (
     <Styled.Wrapper>
-      <Styled.SignButton>-</Styled.SignButton>
+      <Styled.MinusSignButton hasMinValue={hasMinValue}>-</Styled.MinusSignButton>
       <Styled.Amount>{pizzaAmount}</Styled.Amount>
-      <Styled.SignButton>+</Styled.SignButton>
+      <Styled.PlusSignButton hasMaxValue={hasMaxValue}>+</Styled.PlusSignButton>
     </Styled.Wrapper>
   )
 }
